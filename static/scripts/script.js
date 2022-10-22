362d8df6e8195d33624c5a87dd3584fd660a7468
Sofa <Nextil3o3@gmail.com>
2022-10-22 14:34:40 +0200

    Enter key can be pressed to search now

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,7 +1,14 @@
 let searchButton = document.getElementById("sbutton");
+let searchInput = document.getElementById("sbar")
 
 searchButton.addEventListener("click", () => {
-    let text = document.getElementById("sbar")
     searchButton.innerHTML = `<i class="fa fa-search"></i> Searching...`,
-    window.location.href = `/search?query=${text.value}`;
+    window.location.href = `/search?query=${searchInput.value}`;
+});
+
+searchInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        searchButton.innerHTML = `<i class="fa fa-search"></i> Searching...`,
+        window.location.href = `/search?query=${searchInput.value}`;
+    }
 });
