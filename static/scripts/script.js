471a81cff397ce88d301dd140ef5367a1eaa5338
Sofa <Nextil3o3@gmail.com>
2022-10-22 13:31:14 +0200

    Ui changes

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,5 +2,6 @@ let searchButton = document.getElementById("sbutton");
 
 searchButton.addEventListener("click", () => {
     let text = document.getElementById("sbar")
+    searchButton.innerHTML = `<i class="fa fa-search"></i> Searching...`,
     window.location.href = `/search?query=${text.value}`;
 });
--- static/scripts/search.js
+++ static/scripts/search.js
@@ -1,12 +1,20 @@
 let saveButtons = document.getElementsByClassName("save-button");
 
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
+  
+
 for (let button of saveButtons) {
     button.addEventListener("click", () => {
         let title = document.getElementById("card-title")
         let description = document.getElementById("card-summary")
         fetch(`/bookmark?title=${title.innerHTML}&description=${description.innerHTML}&url=${button.id}`, { method: "POST" })
         .then(() => {
-                alert("Website added to bookmarks!");
+            button.innerHTML = `<i class="fa fa-check"></i>`
+            delay(500).then(() => {
+                button.innerHTML = `<i class="fa fa-bookmark"></i>`
+            })
             });
             }    
     );
