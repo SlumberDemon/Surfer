a51c8b4997c4d2584ec8f00f3c0916b2852372cc
Sofa <Nextil3o3@gmail.com>
2022-10-22 15:41:38 +0200

    search buttons fix

--- static/scripts/history.js
+++ static/scripts/history.js
@@ -1,16 +1,18 @@
 let deleteButtons = document.getElementsByClassName("remove-button");
-let searchButton = document.getElementsByClassName("search-button");
+let searchButtons = document.getElementsByClassName("search-button");
 
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
 
-searchButton.addEventListener("click", () => {
+for (let button of searchButtons) {
+button.addEventListener("click", () => {
     let query = document.getElementById("card-title")
-    searchButton.innerHTML = `<i class="fa fa-search"></i> Searching...`,
+    button.innerHTML = `<i class="fa fa-search"></i> Searching...`,
     window.location.href = `/search?query=${query.innerHTML}`;
 });
+}
 
 for (let button of deleteButtons) {
     button.addEventListener("click", () => {
