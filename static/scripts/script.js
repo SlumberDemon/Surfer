434e8c5570d4552d9f8d8700ed6102a22809180b
Sofa <Nextil3o3@gmail.com>
2022-10-22 11:55:50 +0200

    removed consol log thing

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,6 +2,5 @@ let searchButton = document.getElementById("sbutton");
 
 searchButton.addEventListener("click", () => {
     let text = document.getElementById("sbar")
-    console.log(text.value)
     window.location.href = `/search?query=${text.value}`;
 });
