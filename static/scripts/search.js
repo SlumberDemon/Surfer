b3be780cf489bedd2aee64e07af24773830507e5
Sofa <Nextil3o3@gmail.com>
2022-10-25 11:13:50 +0200

    Ui changes and fixed bookmarks

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -7,10 +7,9 @@ function delay(time) {
 
 for (let button of saveButtons) {
     button.addEventListener("click", () => {
-      let title = document.getElementById(`${button.id}ctitle`)
-      console.log(title)
-      let description = document.getElementById(`${button.id}csummary`)
-      fetch(`/bookmark?title=${title.innerHTML}&description=${description.innerHTML}&url=${button.value}`, { method: "POST" })
+      let title = document.getElementById(`${button.id.split("~")[0]}ctitle`)
+      let description = document.getElementById(`${button.id.split("~")[0]}csummary`)
+      fetch(`/bookmark?title=${title.innerHTML}&description=${description.innerHTML}&url=${button.id.split("~")[1]}`, { method: "POST" })
       .then(() => {
         button.innerHTML = `<i class="fa fa-check"></i>`
         delay(500).then(() => {
